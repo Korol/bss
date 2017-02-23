5b20dfedda0ae7e39362a6d92add0e6e88aab841
Andrey Korol <andkorol@gmail.com>
2017-02-23 17:01:24 +0200

    New top menu color, add favicon.ico to Admin area.

--- modules/admin/views/layouts/admin.php
+++ modules/admin/views/layouts/admin.php
@@ -142,7 +142,7 @@ $menu_user = (Yii::$app->user->can('admin')) ? 'admin' : ((Yii::$app->user->can(
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
-    <link rel="shortcut icon" href="<?=\yii\helpers\Url::to(['images/favicon.ico']); ?>" type="image/x-icon">
+    <link rel="shortcut icon" href="<?=\yii\helpers\Url::to(['/images/favicon.ico']); ?>" type="image/x-icon">
     <?php $this->head() ?>
 </head>
 <body>
